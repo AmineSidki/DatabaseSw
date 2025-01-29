7b6b353543b20720e90895ede6c04dc810bd1e13
AmineSidki <amineyoutubw@gmail.com>
2025-01-29 21:37:45 +0100

    Update Parser.java
    removed LL class

--- Parser.java
+++ Parser.java
@@ -1,30 +1,6 @@
 import java.io.*;
 import java.util.*;
 
-class LL{
-    String value;
-    LL next;
-    LL(String str)
-    {
-        value = str;
-        next = null;
-    }
-    void add_node(String str)
-    {
-        if(next == null)
-        {
-            next = new LL(str);
-        }
-        else{
-            LL curr = next ;
-            while(curr.next != null)
-            {
-                curr = curr.next;
-            }
-            curr.next = new LL(str);
-        }
-    }
-}
 
 public class Parser {
 
