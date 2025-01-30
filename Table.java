4de6184209a24a4c258a626b7330e48cf06c3c80
AmineSidki <amineyoutubw@gmail.com>
2025-01-30 23:03:16 +0100

    added an insert function to the Table class

--- Table.java
+++ Table.java
@@ -3,18 +3,119 @@ import java.util.ArrayList;
 
 public class Table {
     String Name;
+    String cdb;
     String[] Columns;
     String[] DataTypes;
 
-    Table(String Table_Name)
+    Table(String Table_Name , String current_db)
     {
         Name = Table_Name;
+        cdb = current_db;
     }
-    public void make(String current_db,String[] args) throws IOException
+    public void insert(String[] args) throws IOException
     {
-        File dbd = new File(current_db + "/" + current_db +".dbd");
+        //we verify if the data has the same number of columns as the table
+        File table = new File(cdb + "/" + Name + ".dbt");
+        ArrayList<String> Values = new ArrayList<>();
+        for(int i = 4 ; i < args.length && !args[i].equals("$") ; i++)
+        {
+            Values.add(args[i]);
+        }
+
+        try(FileReader fr = new FileReader(cdb + "/" + cdb + ".dbd"))
+        {
+            String dbd_file_text = "";
+            boolean CanRead = true;
+            while(CanRead)
+            {
+                int ch = fr.read();
+                if(ch > 0)
+                {
+                    dbd_file_text += (char)ch;
+                }
+                else{
+                    CanRead = false;
+                    fr.close();
+                }
+            }
+            String[] rows = dbd_file_text.split("\n");
+            String[] TName ;
+            int i = 0;
+            do{
+                TName = rows[i].split(":");
+            }while(!TName[0].equalsIgnoreCase(Name) && i++ < rows.length);
+
+            if(Str_to_int(TName[1]) == Values.size()){
+                for(i = 0 ; i < Values.size() ; i++)
+                {
+                    if((i+1) % 3 == 0 && !Values.get(i).equals(","))
+                    {
+                        System.out.println("Error : Expected ',' near argument " + Values.get(i-1));
+                        System.exit(0);
+                    }
+                }
+
+                String[] TCol = TName[2].split("#");
+
+                for(String str1 : Values)
+                {
+                   for(String str2 : TCol)
+                   {
+                       String[] tmp = str2.split("$");
+                       switch(tmp[1])
+                       {
+                           case "INT" :
+                               if(!isInt(str1))
+                               {
+                                   err_inv_type("INT" , str1);
+                               }
+                               break;
+                           case "FLOAT":
+                               if(!isFloat(str1))
+                               {
+                                   err_inv_type("FLOAT" , str1);
+                               }
+                               break;
+                           case "DATE":
+                               if(!isDate(str1))
+                               {
+                                   err_inv_type("DATE" ,str1);
+                               }
+                               break;
+                       }
+                   }
+                }
+
+                try(FileWriter fw = new FileWriter(cdb + "/" + Name + ".dbt"))
+                {
+                    for(String e : Values)
+                    {
+                        fw.write(e);
+                        fw.write("#$#");
+                    }
+                    fw.write("\n");
+                }catch (IOException ioe)
+                {
+                    System.out.println("An error occurred while opening the database file .");
+                    System.exit(0);
+                }
+            }
+            else{
+                System.out.println("Error : Column and data dimensions do not match !");
+                System.exit(0);
+            }
+
+            //if there was no file we return an error.
+        }catch (IOException ioe)
+        {
+            System.out.println("Error : No dbd file , corrupted database.");
+        }
+    }
+    public void make(String[] args) throws IOException
+    {
+        File dbd = new File(cdb + "/" + cdb +".dbd");
         //we then make a file named after the table , which will contain all the rows.
-        File table = new File(current_db + "/" + args[2] + ".dbt");
+        File table = new File(cdb + "/" + Name + ".dbt");
         try {
             boolean b = table.createNewFile();
             if(b == false)
@@ -43,7 +144,8 @@ public class Table {
 
                 for(String e : DataTypes)
                 {
-                    if(e.equals(args[index].toUpperCase()))
+                    args[index] = args[index].toUpperCase();
+                    if(e.equals(args[index]))
                     {
                         AcceptableType = true;
                     }
@@ -72,19 +174,19 @@ public class Table {
         int index = 1;
         for (LL curr = columns; curr != null; curr = curr.next , index++) {
             if (index % 3 == 0 && !curr.value.equals(",")) {
-                err_del_table("Expected ',' near argument " + curr.value + " " + index + args[3+index] , table);
+                err_del_table("Error : Expected ',' near argument " + curr.value , table);
             }
         }
         //if all is conform , we write the columns in the ".dbd" file of the database the
         //following way :
         // Col1$DataT1#Col2$DataT2#....#Coln$DataTn End of Line.
         try(FileWriter fw = new FileWriter(dbd , true)){
-            fw.write(args[2] + ":");
+            fw.write(Name + ":" + columns.size()/4 + ":");
             for (LL curr = columns; curr != null; curr = curr.next) {
                 if (!curr.value.equals(",") || curr.next == null) {
                     fw.write(curr.value);
                     char sep;
-                    if ( curr.next != null && !curr.next.value.equals(",")) {
+                    if (curr.next != null && !curr.next.value.equals(",")) {
                         sep = '$';
                     } else {
                         sep = '#';
@@ -113,5 +215,70 @@ public class Table {
             }while(b == false);
         }
         System.exit(0);
+    }
+    private void err_inv_type(String type , String argumnt)
+    {
+        System.out.println("Error : Invalid type at argument : " + argumnt + " , expected " + type);
+        System.exit(0);
+    }
+    private int Str_to_int(String str)
+    {
+        int out = 0;
+        for(int i = 0; i < str.length() ; i++)
+        {
+            if(str.charAt(i) <= '9' && str.charAt(i) >= '0')
+            {
+                out = out * 10 + str.charAt(i) - '0';
+            }
+        }
+        return out;
+    }
+    private boolean isInt(String str)
+    {
+        if(str.isEmpty())
+        {
+            return false;
+        }
+        for(int i = 0 ; i < str.length() ; i++)
+        {
+            if(str.charAt(i) > '9' || str.charAt(i) < '0')
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    private boolean isFloat(String str)
+    {
+        if(str.isEmpty())
+        {
+            return false;
+        }
+        if(isInt(str))
+        {
+            return true;
+        }
+        int point_counter = 0;
+        for(int i = 0; i < str.length() ; i++)
+        {
+            if((str.charAt(i) > '9' || str.charAt(i) < '0') && (str.charAt(i) != '.' || point_counter > 0))
+            {
+                return false;
+            }
+            if(str.charAt(i) != '.')
+            {
+                point_counter++ ;
+            }
+        }
+        return true;
+    }
+    private boolean isDate(String str)
+    {
+        String[] splitted = str.split("/");
+        if(splitted.length != 3 || splitted[0].length() > 2 || splitted[1].length() > 2 || splitted[2].length() > 4)
+        {
+            return false;
+        }
+        return true;
     }
 }
